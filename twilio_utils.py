ededa56dd86e52036c3692c208bb09ba3dd02b4c
Priyanshu Kumar <114929441+priyanshkrglbitm@users.noreply.github.com>
2025-07-18 11:36:09 +0530

    Update twilio_utils.py

--- twilio_utils.py
+++ twilio_utils.py
@@ -1,10 +1,11 @@
+# twilio_utils.py
 from twilio.rest import Client
-import os
+from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHATSAPP_NUMBER, MY_WHATSAPP_NUMBER
 
 def send_whatsapp(message: str):
-    client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     client.messages.create(
         body=message,
-        from_=os.getenv("TWILIO_WHATSAPP_NUMBER"),
-        to=os.getenv("MY_WHATSAPP_NUMBER")
+        from_=TWILIO_WHATSAPP_NUMBER,
+        to=MY_WHATSAPP_NUMBER
     )
